7f36dd2ca310b2b10fcc5eeef20ddef83f2eb943
TomiXRM <chocolate0785lego@icloud.com>
2022-10-14 17:50:33 +0900

    動いた

---
+++ bldc/src/main.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+
+#include <SimpleFOC.h>
+
+// magnetic sensor instance - SPI
+MagneticSensorSPI sensor = MagneticSensorSPI(AS5048_SPI, 10);
+// magnetic sensor instance - MagneticSensorI2C
+// MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
+// MagneticSensorAnalog sensor = MagneticSensorAnalog(A1, 14, 1020);
+
+// BLDC motor & driver instance
+BLDCMotor motor = BLDCMotor(8);
+BLDCDriver3PWM driver = BLDCDriver3PWM(9, 5, 6, 8);
+// Stepper motor & driver instance
+// StepperMotor motor = StepperMotor(50);
+// StepperDriver4PWM driver = StepperDriver4PWM(9, 5, 10, 6,  8);
+
+// velocity set point variable
+float target_velocity = 0;
+// instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char *cmd) { command.scalar(&target_velocity, cmd); }
+
+void setup() {
+
+    // initialise magnetic sensor hardware
+    sensor.init();
+    // link the motor to the sensor
+    motor.linkSensor(&sensor);
+
+    // driver config
+    // power supply voltage [V]
+    driver.voltage_power_supply = 16;
+    driver.init();
+    // link the motor and the driver
+    motor.linkDriver(&driver);
+
+    // set motion control loop to be used
+    motor.controller = MotionControlType::velocity;
+
+    // contoller configuration
+    // default parameters in defaults.h
+
+    // velocity PI controller parameters
+    motor.PID_velocity.P = 0.1f;
+    motor.PID_velocity.I = 0.1;
+    motor.PID_velocity.D = 0;
+    // default voltage_power_supply
+    motor.voltage_limit = 8;
+    // jerk control using voltage voltage ramp
+    // default value is 300 volts per sec  ~ 0.3V per millisecond
+    motor.PID_velocity.output_ramp = 1000;
+
+    // velocity low pass filtering
+    // default 5ms - try different values to see what is the best.
+    // the lower the less filtered
+    motor.LPF_velocity.Tf = 0.01f;
+
+    // use monitoring with serial
+    Serial.begin(115200);
+    // comment out if not needed needed
+    motor.useMonitoring(Serial);
+
+    // initialize motor
+    motor.init();
+    // align sensor and start FOC
+    motor.initFOC();
+
+    // add target command T
+    command.add('T', doTarget, "target velocity");
+
+    Serial.println(F("Motor ready."));
+    Serial.println(F("Set the target velocity using serial terminal:"));
+    _delay(1000);
+}
+
+float fadeAmout = 0.001;
+void Fading() {
+    target_velocity += fadeAmout;
+    if (target_velocity <= 0.0 || target_velocity >= 70.0) {
+        fadeAmout = -fadeAmout;
+    }
+}
+
+void loop() {
+    Fading();
+    // main FOC algorithm function
+    // the faster you run this function the better
+    // Arduino UNO loop  ~1kHz
+    // Bluepill loop ~10kHz
+    motor.loopFOC();
+
+    // Motion control function
+    // velocity, position or voltage (defined in motor.controller)
+    // this function can be run at much lower frequency than loopFOC() function
+    // You can also use motor.move() and set the motor.target in the code
+    motor.move(target_velocity);
+
+    // function intended to be used with serial plotter to monitor motor variables
+    // significantly slowing the execution down!!!!
+    // motor.monitor();
+
+    // user communication
+    command.run();
+}
