0dd0b633b20edf64589696647bc564d53d001912
tomixrm <chocolate0785lego@icloud.com>
2023-05-29 01:24:54 +0900

    メモの追加

--- BLDC_programV3/Core/Src/main.c
+++ BLDC_programV3/Core/Src/main.c
@@ -269,7 +269,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1800;
+  htim2.Init.Period = 1799;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -328,7 +328,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 1800;
+  htim3.Init.Period = 1799;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
--- BLDC_programV3/Src/code.cpp
+++ BLDC_programV3/Src/code.cpp
@@ -1,33 +1,66 @@
 #include "code.hpp"
 #include <stdio.h>
+/*
+# PWMの周波数に関するCubeMXの設定について
+- HCLK(システムクロック)
+- PSC(プリスケーラ)
+- CP(カウンターピリオド)
 
-void pwm();
+Freq = HCLK / (PSC + 1) * (CP + 1)
+
+# Freqを100Khzにしたい．
+- HCLK = 180MHz
+
+$$ 180MHz / 100KHz = 1800 $$
+$$ 1800 = (PSC + 1) * (CP + 1) $$
+
+- PSC = 0
+- CP = 1799
+
+# デューティ比に関するCubeMXの設定について
+- PWM Mode
+- Channel
+- Pulse
+
+Duty = Pulse / (CP + 1)
+
+# Dutyを50%にしたい．
+- Pulse = 900
+
+$$ 900 = 900 / (1799 + 1) $$
+
+# その他
+- この設定だと，PWMの周波数は100KHz，デューティ比は50%になる．
+- この設定だと，TIM2_CH3(PA2)ピンにPWM信号が出力される．
+
+# 参考
+- https://qiita.com/ShunHattori/items/68f099f1d77702d2535d
+- https://garberas.com/archives/2138
+- https://moons.link/post-632/
+- https://rt-net.jp/mobility/archives/19815
+
+*/
+
 int count = 0;
 int maxValue = 1800;
 
 void setup() {
     printf("hello\n");
+    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3); // PWMの開始
 }
 
 void loop() {
-
-    pwm();
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-}
-
-void pwm() {
-    HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
-    while (1) {
-        HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
-        HAL_Delay(1000);
-        HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-        HAL_Delay(50);
-        if (count > maxValue) {
-            count = 0;
-        } else {
-            count++;
-        }
-
-        printf("pwm%d\n", count);
+    if (count > maxValue) {
+        count = 0;
+    } else {
+        count++;
     }
+    __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, count); // PWMのデューティ比の設定
+
+    printf("pwm%d\n", count);
+
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    HAL_Delay(50);
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+    HAL_Delay(50);
 }
