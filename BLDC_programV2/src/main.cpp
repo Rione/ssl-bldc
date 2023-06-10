b145bb9a0e42cb81ea3ee7104de1215269a0d44f
tomixrm <chocolate0785lego@icloud.com>
2023-06-10 17:25:22 +0900

    :sparkles: add RGB Led
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

--- BLDC_programV2/src/main.cpp
+++ BLDC_programV2/src/main.cpp
@@ -3,61 +3,73 @@
 Serial pc(USBTX, USBRX, 230400); // tx, rx
 DigitalOut led(PC_14);
 
-CAN can(PB_8, PB_9, 100000);
-CANMessage msg;
+DigitalOut red(PB_14);
+DigitalOut green(PB_15);
+DigitalOut blue(PB_13);
 
 char counter = 0;
+
+void rgb(bool r, bool g, bool b) {
+    red = !r;
+    green = !g;
+    blue = !b;
+}
+
 int main() {
     while (1) {
+        // flash rgb led with all patterns
+        rgb(1, 0, 0);
         wait_ms(100);
-        pc.printf(".");
-        // if (can.read(msg)) {
-        //     pc.printf("\nMessage received: %d\n", msg.data[0]);
-        //     // led = !led;
-        // }
-        if (can.write(CANMessage(100, &counter, 1))) {
-            printf("wloop()\n");
-            counter++;
-            printf("Message sent: %d\n", counter);
-            // led = !led;
-        }
+        rgb(0, 1, 0); // pink
+        wait_ms(100);
+        rgb(0, 0, 1);
+        wait_ms(100);
+        rgb(1, 1, 0); // r
+        wait_ms(100);
+        rgb(1, 0, 1);
+        wait_ms(100);
+        rgb(0, 1, 1); // b
+        wait_ms(100);
+        rgb(1, 1, 1);
         led = !led;
+        wait_ms(100);
     }
 }
 
-// // Creates a CAN interface connected to specific pins.
 
-// @param rd read from transmitter
-//     @param td transmit to transmitter
+                   // // Creates a CAN interface connected to specific pins.
 
-// #include "mbed.h"
+                   // @param rd read from transmitter
+                   //     @param td transmit to transmitter
 
-//         Ticker ticker;
-// DigitalOut led1(LED1);
-// DigitalOut led2(LED2);
-// // The constructor takes in RX, and TX pin respectively.
-// // These pins, for this example, are defined in mbed_app.json
-// CAN can1(MBED_CONF_APP_CAN1_RD, MBED_CONF_APP_CAN1_TD);
-// CAN can2(MBED_CONF_APP_CAN2_RD, MBED_CONF_APP_CAN2_TD);
+                   // #include "mbed.h"
 
-// char counter = 0;
+                   //         Ticker ticker;
+                   // DigitalOut led1(LED1);
+                   // DigitalOut led2(LED2);
+                   // // The constructor takes in RX, and TX pin respectively.
+                   // // These pins, for this example, are defined in mbed_app.json
+                   // CAN can1(MBED_CONF_APP_CAN1_RD, MBED_CONF_APP_CAN1_TD);
+                   // CAN can2(MBED_CONF_APP_CAN2_RD, MBED_CONF_APP_CAN2_TD);
 
-// void send() {
-//     if (can1.write(CANMessage(1337, &counter, 1))) {
-//         printf("Message sent: %d\n", counter);
-//         counter++;
-//     }
-//     led1 = !led1;
-// }
+                   // char counter = 0;
 
-// int main() {
-//     ticker.attach(&send, 1);
-//     CANMessage msg;
-//     while (1) {
-//         if (can2.read(msg)) {
-//             printf("Message received: %d\n\n", msg.data[0]);
-//             led2 = !led2;
-//         }
-//         wait(0.2);
-//     }
-// }
+                   // void send() {
+                   //     if (can1.write(CANMessage(1337, &counter, 1))) {
+                   //         printf("Message sent: %d\n", counter);
+                   //         counter++;
+                   //     }
+                   //     led1 = !led1;
+                   // }
+
+                   // int main() {
+                   //     ticker.attach(&send, 1);
+                   //     CANMessage msg;
+                   //     while (1) {
+                   //         if (can2.read(msg)) {
+                   //             printf("Message received: %d\n\n", msg.data[0]);
+                   //             led2 = !led2;
+                   //         }
+                   //         wait(0.2);
+                   //     }
+                   // }
